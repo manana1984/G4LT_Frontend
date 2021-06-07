fc49f16821b05573d77f342772fa67882e36c393
Manana1984 <masatrian1990@gmail.com>
2021-06-08 03:42:19 +0900

    description function fix

--- src/Screens/DescriptionDetails/index.js
+++ src/Screens/DescriptionDetails/index.js
@@ -7,10 +7,12 @@ import { connectAuth, connectGeneralStatesToProps, connectAuthDescription } from
 import styles from './styles';
 import FeedsAPI from '../../Services/feeds';
 
-const DescriptionDetailsScreen = (props) => {
+const DescriptionDetailsScreen = (props, { created_at }) => {
   const { user } = props;
-  const [desText, setDesText] = useState('');
+  const [desText, setDesText] = useState("");
   const [comments, setComments] = useState([]);
+  const [editCommentTxt, setEditCommentTxt] = useState('');
+  const [isEdit, setIsEdit] = useState([]);
   useLayoutEffect(() => {
 
     props.navigation.setOptions({
@@ -18,7 +20,7 @@ const DescriptionDetailsScreen = (props) => {
       headerTitleStyle: { alignSelf: 'center' },
       headerTitle: "Post & Comments",
       headerLeft: () => (
-        <TouchableOpacity onPress={() => props.navigation.navigate('G4LT')} style={{ marginLeft: 10 }}>
+        <TouchableOpacity onPress={() => props.navigation.navigate('discover')} style={{ marginLeft: 10 }}>
           <Ionicons name='chevron-left' size={24} color='Black' />
         </TouchableOpacity>
       ),
@@ -40,33 +42,97 @@ const DescriptionDetailsScreen = (props) => {
   const setComment = data => {
     const temp = [...comments];
     temp.push(data);
-    setDesText('');
+    setDesText("");
+    setComments(temp);
+    setIsEdit(isEdit.concat(false));
+  }
+
+  const commentDelete = (index) => {
+    const temp = [...comments];
+    temp.splice(index, 1);
     setComments(temp);
   }
 
+  const commentEdit = (index) => {
+    if (!isEdit[index]) {
+      const temp = [...isEdit];
+      temp[index] = !isEdit[index];
+      setIsEdit(temp);
+      setEditCommentTxt(comments[index]);
+    }
+    else {
+      let tempEdit = [...isEdit];
+      tempEdit[index] = !isEdit[index];
+      setIsEdit(tempEdit);
+      const temp = [...comments];
+      temp[index] = editCommentTxt;
+      setComments(temp);
+    }
+  }
+
   return (
+
     <View style={styles.MainContainer}>
-      <View>
-        <Text style={styles.input}> {props.route.params.description}</Text>
-      </View>
-      <View style={{ flexDirection: 'row', flexWrap: 'wrap', marginLeft: 23 }} >
-        {
-          props.route.params.attachments.includes(',data') ? props.route.params.attachments.split(',data').map((item, index) => (
-            <Image style={styles.tinyLogo} source={{ uri: index === 0 ? item : `data${item}` }} />
-          )) : (
-            <Image style={styles.tinyLogo} source={{ uri: props.route.params.attachments }} />
-          )
-        }
-        {
-          comments.map((comment, index) => (
-          <Text style={styles.input2} key={comment}>{comments}</Text>
-          ))
-        }
-      </View>
+      <ScrollView>
+        <View>
+          <Text style={styles.input}> {props.route.params.description}</Text>
+        </View>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap', marginLeft: 23 }} >
+          {
+            props.route.params.attachments.includes(',data') ? props.route.params.attachments.split(',data').map((item, index) => (
+              <Image style={styles.tinyLogo} source={{ uri: index === 0 ? item : `data${item}` }} />
+            )) : (
+              <Image style={styles.tinyLogo1} source={{ uri: props.route.params.attachments }} />
+            )
+          }
+          {
+            comments.map((comment, index) => (
+              <View>
+                <View style={styles.comment}>
+                  <TouchableOpacity style={styles.Avatar} >
+                    <Avatar size="small" icon={{ name: 'user', type: 'font-awesome' }} activeOpacity={0.1} rounded
+                      source={{ uri: user.avatar || 'https://faces/twitter/ladylexy/128.jpg', }} />
+                  </TouchableOpacity>
+                  <Text style={styles.name}> {user.firstname} {user.lastname}  </Text>
+                  <View>{created_at}</View>
+                </View>
+                <View style={styles.comment1}>
+                  {isEdit[index] ?
+                    // <View style={styles.input2}>
+                      <TextInput multiline={true} numberOfLines={0} style={styles.input3}
+                        onChangeText={x => setEditCommentTxt(x)} value={editCommentTxt} />
+                    // </View>
+                    :
+                    <Text style={styles.input2} key={comment}>{comment}</Text>
+                  }
+                  {isEdit[index] ?
+                    <TouchableOpacity style={styles.edit} onPress={() => commentEdit(index)}>
+                      <Ionicons name='check' size={17} color='#800080' />
+                    </TouchableOpacity> :
+                    <TouchableOpacity style={styles.edit} onPress={() => commentEdit(index)}>
+                      <Ionicons name='edit' size={17} color='#800080' />
+                    </TouchableOpacity>
+                  }
+                  <TouchableOpacity style={styles.delete} onPress={() => commentDelete(index)}>
+                    <Ionicons name='delete' size={17} color='#800080' />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ))
+          }
+        </View>
+      </ScrollView>
       <View style={styles.line}  >
         <View style={styles.input1}>
           <TextInput multiline={true} numberOfLines={4} placeholder="Start typing..."
-            onChangeText={x => setDesText(x)} value={desText} />
+            onChangeText={x => {
+              setDesText(x);
+              let temp = [];
+              for (let i = 0; i < isEdit.length; i++) {
+                temp.push(false);
+              }
+              setIsEdit(temp);
+            }} value={desText} />
         </View>
         <View style={styles.icon1}>
           {/* <Ionicons name='plus-circle' size={27} color='#800080' /> */}
--- src/Screens/DescriptionDetails/styles.js
+++ src/Screens/DescriptionDetails/styles.js
@@ -3,7 +3,7 @@ const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 export default StyleSheet.create({
-    
+
     Avatar: {
         display: 'flex',
         alignItems: 'center',
@@ -11,41 +11,71 @@ export default StyleSheet.create({
         paddingRight: 5
     },
     input: {
-        width: width / 6 * 5 ,
+        width: width / 6 * 5,
         paddingHorizontal: 17,
         marginLeft: 15,
         fontSize: 17,
         marginTop: 15
-    },   
+    },
     input1: {
         width: width / 6 * 5,
         paddingHorizontal: 17,
         marginLeft: 15,
         fontSize: 17
-    },   
+    },
     input2: {
-        width: width / 6 * 5,
+        width: width /25 * 17,
         paddingHorizontal: 17,
-        marginLeft: 15,
+        marginLeft: 40,
         fontSize: 17,
-        marginTop: 10
-    },   
+        marginTop: -8,
+        marginBottom: 8
+    },
+    input3: {
+        width: width /25 * 17,
+        paddingHorizontal: 17,
+        marginLeft: 40,
+        fontSize: 17,
+        marginTop: -19,
+        marginBottom: 8
+    },
     MainContainer: {
         flex: 1,
         // alignItems: 'center',
         // justifyContent: 'center',        
-        paddingTop: ( Platform.OS === 'ios' ) ? 20 : 0
+        paddingTop: (Platform.OS === 'ios') ? 20 : 0,
     },
     line: {
         display: 'flex',
         flexDirection: 'row',
-        width: '100%', 
-        height: 50, 
-        justifyContent: 'center', 
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
         alignItems: 'center',
         position: 'absolute',
         bottom: 0,
         marginLeft: 15
+    },
+    comment: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        width: width* 1,
+        marginRight: -100,
+        marginTop: 15
+        // paddingEnd: 10
+    },
+    comment1: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        width: width* 1,
+        marginRight: -100,
+        marginTop: 5
+        // paddingEnd: 10
+        // flex: 1,
+        // paddingLeft: 10,
+        // paddingRight: 10,
     },
     tinyLogo: {
         // alignItems: 'center',
@@ -56,12 +86,43 @@ export default StyleSheet.create({
         // borderRadius:20,
         borderWidth: .1,
         borderColor: 'grey',
-        margin: 20
+        marginLeft: 20,
+        marginTop: 9
+    },
+    tinyLogo1: {
+        // alignItems: 'center',
+        // margin: 10,
+        marginLeft: 7,
+        width: 310,
+        height: 310,
+        // borderRadius:20,
+        borderWidth: .1,
+        borderColor: 'grey',
+        margin: 20,
+        marginLeft: 23
     },
     icon1: {
         marginRight: 3
     },
     icon2: {
         marginRight: 50
+    },
+    edit:{
+        marginTop: -6,
+        paddingRight: 3 
+    },
+    delete:{
+        marginTop: -6,
+        marginLeft: 3
+        // marginRight: 20
+    },
+    name: {
+        fontWeight: "bold",
+        fontSize: 19,
+        marginLeft: 5
+    },
+    textInputStyle:{
+       height: 50,
+       width: width /25 * 17,
     }
 });
